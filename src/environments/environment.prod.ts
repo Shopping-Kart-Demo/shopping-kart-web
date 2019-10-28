ba5483a0d2d41cc344b27327354a7aa497d0293b
AndresSp <andrestunonsp@gmail.com>
2019-10-28 11:46:11 -0500

    Update to deploy in heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  host: "https://shopping-kart-api.herokuapp.com",
+  apikeyRecaptchaV2: "6Ldfab0UAAAAALrt2ZIUQoUbRxKJw16QfyJ1PTF3",
+  apikeyRecaptchaV3: "6LdGlb4UAAAAAENamOv8fapsDpbYGfyy4k9ckLI2",
+  apikeyRecaptchaInvisibleV2: "6LdGPb4UAAAAAGY-m4rLbYV4JZYyNjGiyYTbOjKB",
+  apikeyRecaptchaV2Register: "6LfxYr8UAAAAAPlPMRfpEjI6I3C5jscrrvVmg5wp"
 };
